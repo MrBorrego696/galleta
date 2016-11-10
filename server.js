3478c4cb94286787314c8ea51fbb8302c0d94ec8
computo1-pc8 <computo1-pc8>
2016-11-10 08:33:46 -0600

    bower

---
+++ server.js
@@ -0,0 +1,30 @@
+var http = require('http');
+//Cargando configuraciones
+var config = require('./config/config.js');
+var PORT = config.PORT;
+var IP = config.IP;
+var fs = require('fs');
+var mime = require('mime');
+var path = require('path');
+var staticServer = require('./internals/static-server.js');
+
+//Agregar paqueteria de colores
+//Para importar los colores
+var colors = require('colors');
+//Tema de colors....
+colors.setTheme(config.color_theme);
+//req       peticion
+//res       respuesta
+var server = http.createServer(function (req, res) {
+    var urlPath = req.url;
+    if (urlPath === '/') {
+        urlPath = ('/index.html');
+    }
+    //Se llama al servidor estatico
+    staticServer.serve(urlPath, res);
+});
+
+server.listen(PORT, IP, function () {
+    console.log(`>Server working @http://${IP}:${PORT}`.info);
+
+});
